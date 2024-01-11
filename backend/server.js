4052112343c2e5863b9f0e2d1de42b7c669f31f9
nasheed.sm <neshinasersm@gmail.com>
2024-01-11 20:21:04 +0530

    socket site changed

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ import("socket.io").then((socketIO) => {
     const io = new socketIO.Server(server, {
         pingTimeout: 60000,  // amount of time it will wait while being inactive. So after one minute of inactivity(not send any message), it will close the connection to save the bandwidth
         cors: {
-            origin: "http://localhost:3000"
+            origin: "https://nahalasm.shop"
         }
     });
     
